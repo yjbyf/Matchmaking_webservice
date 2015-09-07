7f59b95a254bd557aa9871e7ee0d871b2e1ab91a
baiyufan <baiyufan@gmail.com>
2015-09-07 23:30:33 +0800

    init project

---
+++ src/main/java/com/baiyufan/system/User.java
@@ -0,0 +1,35 @@
+package com.baiyufan.system;
+
+import org.springframework.data.annotation.Id;
+
+public class User {
+
+	@Id private String id;
+
+	private String userName;
+	private String password;
+	private String group;
+	
+	public String getGroup() {
+		return group;
+	}
+	public void setGroup(String group) {
+		this.group = group;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+}
---
+++ src/main/java/com/baiyufan/system/UserRepository.java
@@ -0,0 +1,15 @@
+
+package com.baiyufan.system;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "user", path = "user")
+public interface UserRepository extends MongoRepository<User, String> {
+
+	List<User> findByUserName(@Param("name") String name);
+
+}
