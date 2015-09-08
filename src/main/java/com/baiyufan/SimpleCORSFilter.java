f373d12dfb4bca343ee4f9acf89094f4aa488572
baiyufan <baiyufan@gmail.com>
2015-09-08 23:29:25 +0800

    加入patch支持

--- src/main/java/com/baiyufan/SimpleCORSFilter.java
+++ src/main/java/com/baiyufan/SimpleCORSFilter.java
@@ -18,7 +18,7 @@ public class SimpleCORSFilter implements Filter {
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods",
-				"POST, GET, OPTIONS, DELETE");
+				"POST, GET, OPTIONS, DELETE,PATCH");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type");
 		chain.doFilter(req, res);
