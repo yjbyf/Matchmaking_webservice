c1501ee8d33d381a23ac51d8ec6f3cce9b376ff8
baiyufan <baiyufan@baosight.com>
2015-09-09 23:16:23 +0800

    过滤器中加入安全认证机制，请求rest webservice时必须给出用户名和密码

--- src/main/java/com/baiyufan/SimpleCORSFilter.java
+++ src/main/java/com/baiyufan/SimpleCORSFilter.java
@@ -1,13 +1,17 @@
 package com.baiyufan;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,8 +24,27 @@ public class SimpleCORSFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Methods",
 				"POST, GET, OPTIONS, DELETE,PATCH");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type");
-		chain.doFilter(req, res);
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, accept, authorization");
+		//return;
+		
+		HttpServletRequest request = (HttpServletRequest) req;
+
+		String auth = request.getHeader("Authorization");
+
+		System.err.println(auth);
+		// TODO
+		// 判断传送过来的用户名和密码是否符合数据中的
+		// 1.符合，则
+		if ("aa".equals(auth)) {
+			chain.doFilter(req, res);
+		}else{
+			// 2.不符合，则do nothing
+			//PrintWriter out = response.getWriter();
+			//out.println("{}");
+		}
+		
+		
+		//chain.doFilter(req, res);
 	}
 
 	public void init(FilterConfig filterConfig) {
