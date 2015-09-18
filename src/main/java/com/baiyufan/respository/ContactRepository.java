19b01f9870095f14106050caa8b69b0a065f4a30
baiyufan <baiyufan@baosight.com>
2015-09-18 16:47:22 +0800

    修改rest webservice根目录为rest

--- src/main/java/com/baiyufan/respository/ContactRepository.java
+++ src/main/java/com/baiyufan/respository/ContactRepository.java
@@ -1,10 +1,7 @@
 
 package com.baiyufan.respository;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.baiyufan.utils.Constants;
