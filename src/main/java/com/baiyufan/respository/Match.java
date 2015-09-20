a99af483f3412c31225903f82c7ee8d2078e874b
baiyufan <baiyufan@baosight.com>
2015-09-20 19:44:01 +0800

    配对初步后台功能

--- src/main/java/com/baiyufan/respository/Match.java
+++ src/main/java/com/baiyufan/respository/Match.java
@@ -9,12 +9,73 @@ public class Match {
 
 	private String matchDate;// 配对日期 
 	private String name;//姓名
-	private String fee;//收费
-	private String checkEmployee;//买单老师 
-	private float serviceTimes;//服务次数 
+	//private String fee;//收费
+	//private String checkEmployee;//买单老师 
+	//private float serviceTimes;//服务次数 
 	private String serviceEmployee;//服务老师 
 	private String matchPerson;//配对对象 
 	private String visitResult;//回访结果 还在进行，结束，成功 
 	private String visitRemark;//回访情况说明
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPk() {
+		return id;
+	}
+	public void setPk(String pk) {
+		this.pk = null;
+	}
+	public String getMatchDate() {
+		return matchDate;
+	}
+	public void setMatchDate(String matchDate) {
+		this.matchDate = matchDate;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// public String getFee() {
+	// return fee;
+	// }
+	// public void setFee(String fee) {
+	// this.fee = fee;
+	// }
+	// public String getCheckEmployee() {
+	// return checkEmployee;
+	// }
+	// public void setCheckEmployee(String checkEmployee) {
+	// this.checkEmployee = checkEmployee;
+	// }
+	public String getServiceEmployee() {
+		return serviceEmployee;
+	}
+	public void setServiceEmployee(String serviceEmployee) {
+		this.serviceEmployee = serviceEmployee;
+	}
+	public String getMatchPerson() {
+		return matchPerson;
+	}
+	public void setMatchPerson(String matchPerson) {
+		this.matchPerson = matchPerson;
+	}
+	public String getVisitResult() {
+		return visitResult;
+	}
+	public void setVisitResult(String visitResult) {
+		this.visitResult = visitResult;
+	}
+	public String getVisitRemark() {
+		return visitRemark;
+	}
+	public void setVisitRemark(String visitRemark) {
+		this.visitRemark = visitRemark;
+	}
 
 }
--- src/main/java/com/baiyufan/utils/Constants.java
+++ src/main/java/com/baiyufan/utils/Constants.java
@@ -17,36 +17,33 @@ public class Constants {
 
 	public final static String URL_NOPRIV = "open";
 	public final static String URL_PRIV = "rest";
-
+	//no privilage url
 	public final static String LOGIN_VALID_PRE_WITH_SLASH = SLASH + URL_NOPRIV
 			+ SLASH + LOGIN_VALID;
 
 	public final static String CHANGE_OWN_PASSWORD = SLASH + URL_NOPRIV + SLASH
 			+ "change_own_password";
-
+	
+	public final static String USER_REST_WEBSERVICE_NO_PRIV = SLASH
+			+ URL_NOPRIV + SLASH + "userListNoPriv";
+	
+	//user
 	public final static String USER_REST_WEBSERVICE_PATH = "user";
 
 	public final static String USER_REST_WEBSERVICE_PATH_PRE_WTIH_SLASH = SLASH
 			+ URL_PRIV + SLASH + USER_REST_WEBSERVICE_PATH;
-
-	public final static String USER_REST_WEBSERVICE_NO_PRIV = SLASH
-			+ URL_NOPRIV + SLASH + "userListNoPriv";
-
-	// public final static String GET_USER_LIST_WITH_PRIV = SLASH +
-	// "userWithPriv";
 
 	public final static String USER_REST_WEBSERVICE_COUNT = SLASH + URL_PRIV
 			+ SLASH + USER_REST_WEBSERVICE_PATH + SLASH + "count";
 
 	public final static String USER_REST_WEBSERVICE_CHANGE_PASSWORD = USER_REST_WEBSERVICE_PATH
 			+ SLASH + "changePassword";
-
+	// person
 	public final static String PERSON_REST_WEBSERVICE_PATH = "person";
 
 	public final static String CONTACT_REST_WEBSERVICE_PATH = "contact";
 
-	public final static String EMPLOYEE_REST_WEBSERVICE_PATH = "employee";
-
+	// contract
 	public final static String CONTRACT_REST_WEBSERVICE_PATH = "contract";
 
 	public final static String CONTRACT_SAVE_REST_WEBSERVICE_PATH = SLASH
@@ -54,9 +51,15 @@ public class Constants {
 
 	public final static String CONTRACT_VALID_REST_WEBSERVICE_PATH = SLASH
 			+ URL_PRIV + SLASH + CONTRACT_REST_WEBSERVICE_PATH + "/valid";
+	// employee
+	public final static String EMPLOYEE_REST_WEBSERVICE_PATH = "employee";
 
 	public final static String EMPLOYEE_REST_WEBSERVICE_PATH_PRE_WTIH_SLASH = SLASH
 			+ EMPLOYEE_REST_WEBSERVICE_PATH;
+	
+	//match
+	public final static String MATCH_REST_WEBSERVICE_PATH = "/match";
+
 	// ///////////////////////////////////////////////////////////////////////////////////////////////////
 	// admin init password
 	public final static String ADMIN = "admin";
